46692ea4326115d2d6f69dd350d2956cb13ab30c
gizemcoban <gizem.coban.r@gmail.com>
2024-11-12 16:59:46 +0300

    transactions are added.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import './App.css'
 import './components/Profile';
 import userData from "./userData.json";
 import friends from "./friends.json"
+import transactions from "./transactions.json"
 import Profile  from './components/Profile';
 import FriendList from './components/FriendList';
+import TransactionHistory from './components/TransactionHistory';
 
 
 
@@ -18,6 +20,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
     </>
 
   );
