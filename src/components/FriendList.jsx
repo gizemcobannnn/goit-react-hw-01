32b3bc8f42f0677dbe1fb3ac46252d60c5753c7f
gizemcoban <gizem.coban.r@gmail.com>
2024-11-12 14:42:14 +0300

    styles are fixed

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,18 +1,22 @@
+import FriendListItem from './FriendListItem';
+import friends from '../friends.json';
+
 const FriendList = () => {
   return (
     <div>
-        <ul>
-            <li>
-            <div>
-                <img src="" alt="Avatar" width="48" />
-                <p>Friend name</p>
-                 <p>Friend status</p>
-            </div>
-
-	        </li>
+        <ul style={{display:"flex", justifyContent:"center", listStyle:"none",listStyleType:"none"}}>
+        {friends.map(friend => (
+      <li style={{ margin:'1em', display:"flex", alignItems:"baseline",border:"solid 2px black", padding:"5px 20px"}} key={friend.id}>
+        <FriendListItem 
+          name={friend.name} 
+          avatar={friend.avatar} 
+          isOnline={friend.isOnline} 
+        />
+      </li>
+        ))}
         </ul>
     </div>
   )
 }
 
-export default FriendList
+export default FriendList;
