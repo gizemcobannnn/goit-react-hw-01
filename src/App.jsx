3eade1df9bff68ac1843fd8bd1dacd02d9ca4363
gizemcoban <gizem.coban.r@gmail.com>
2024-11-12 12:18:04 +0300

    user json data

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import './components/Profile';
+import userData from "../userData.json";
+import Profile  from './components/Profile';
+
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+
+export default App
